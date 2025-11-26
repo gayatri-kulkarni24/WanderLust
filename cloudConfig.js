608be2be2c897dc194468dbb99100632820537e3
gayatri-kulkarni24 <gaytrikulkarni616@gmail.com>
2025-11-26 18:07:35 +0530

    Update cloudConfig.js
    updated cloudConfig.js

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,23 +1,19 @@
-// const cloudinary = require('cloudinary').v2;
-// const { CloudinaryStorage } = require('multer-storage-cloudinary');
 import { CloudinaryStorage } from "@cloudinary/multer-storage-cloudinary";
 import multer from "multer";
 import { v2 as cloudinary } from "cloudinary";
+
 cloudinary.config({
-    cloud_name:process.env.CLOUD_NAME,
-    api_key:process.env.CLOUD_API_KEY,
-    api_secret:process.env.CLOUD_API_SECRET,
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
 });
 
 const storage = new CloudinaryStorage({
-    cloudinary: cloudinary,
-    params: {
-      folder: 'wanderlust_DEV',
-      allowedFormats:["png","jpg","jpeg"],
-    },
-  });
+  cloudinary: cloudinary,
+  params: {
+    folder: "wanderlust_DEV",
+    allowedFormats: ["png", "jpg", "jpeg"],
+  },
+});
 
-  module.exports={
-    cloudinary,
-    storage,
-  };
+export { cloudinary, storage };
